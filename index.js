098bc4e72d9212837c927d1357b2f3183a31a065
ICE0208 <doky3307@naver.com>
2024-10-01 02:35:10 +0900

    연습 파일 세팅

--- index.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-
-const app = express();
-
-app.get("/", (req, res) => {
-    res.send("🐳 Web Server For Docker!")
-})
-
-app.listen(3000, ()=>console.log("Server is running!"))
