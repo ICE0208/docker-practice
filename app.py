58609c07f494f1e967e35de0e3e01fc85e8d21a3
ICE0208 <doky3307@naver.com>
2024-10-01 23:45:29 +0900

    자동 배포 테스트

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Auto Deploy Change'
+    return 'Auto Deploy Change (One More)'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
